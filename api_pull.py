020692657761bdd9f8ad9e88b673945cf861a2d8
mystyJL <justinl8@hotmail.ca>
2025-12-09 13:57:47 -0800

    updated for new api

--- api_pull.py
+++ api_pull.py
@@ -1,8 +1,10 @@
 import requests
 import time
 # import tqdm
-httpurl = "https://www.nexon.com/api/maplestory/no-auth/v1/ranking/na?type=overall&id=legendary&reboot_index=0&page_index="
-real = 24494210
+#          https://www.nexon.com/api/maplestory/no-auth/ranking/v2/na?type=overall&id=weekly&reboot_index=0&page_index=1
+httpurl = "https://www.nexon.com/api/maplestory/no-auth/ranking/v2/na?type=overall&id=legendary&reboot_index=0&page_index="
+real = int(requests.get(httpurl+str(0)).json()["totalCount"])
+worldid = {1:"bera",19:"Scania",45:"Kronos",70:"Hyperion"}
 classes = {}
 counter= 0
 checkBreak = False
@@ -13,10 +15,11 @@ for i in range(0,real,10):
     jsonData = response.json()
     players = jsonData["ranks"]
     for player in players:
-        if int(player["level"])<280:
+        if int(player["level"])<270:
             checkBreak = True
             break
-        print(player)
+        new = {"ign":player["characterName"],"lvl":player["level"], "job":player["jobName"], "world": worldid[int(player["worldID"])]}
+        print(new)
     if checkBreak:
         break
     time.sleep(1)
--- statistics_nexon.py
+++ statistics_nexon.py
@@ -11,42 +11,28 @@ file = open("playerData.txt","r",encoding="UTF-8")
 text = file.read()
 
 # clean the data
-json_strings = text.strip().replace("'","\"").replace("False,","\"False\",").split("\n")
+json_strings = text.strip().replace("'",'"').split("\n")
+
 for i in range(len(json_strings)):
     json_strings[i] = json.loads(json_strings[i])
 
 
 # 280,285,290,295 respectively
-classes_all = [{},{},{},{}]
+classes_all = [{},{},{},{},{},{},{}]
 class_name = set()
 
 # count the jobs
 for i in json_strings:
-    maple_class = (i["jobName"],i["jobDetail"])
-    # get a list of class names for later
-    if maple_class in class_rename:
-        class_name.add(class_rename[maple_class])
-    else:
-        class_name.add(i["jobName"])
+    maple_class = i["job"]
+    class_name.add(maple_class)
     # sort the data
-    for j in range(4):
-        if int(i["level"]) >= 280+(j*5):
+    for j in range(7):
+        if int(i["lvl"]) >= 270+(j*5):
             if maple_class not in classes_all[j]:
                 classes_all[j][maple_class] = 0
             classes_all[j][maple_class] += 1
+print(classes_all)
 
-
-# clean up class names
-for index in range(4):
-    new_dict = {}
-    for i,j in classes_all[index].items():
-        if i in class_rename:
-            new_dict[class_rename[i]] = j
-        else:
-            if i[0] not in new_dict:
-                new_dict[i[0]] = 0
-            new_dict[i[0]] += j
-    classes_all[index] = new_dict
 
 class_name = list(class_name)
 class_name.sort()
@@ -55,19 +41,22 @@ class_name.sort()
 
 # format for markdown table and print
 
-formatting_short = "|{}\t\t\t\t|{}\t|{}\t|{}\t|{}|"
-formatting_long = "|{}\t\t\t|{}\t|{}\t|{}\t|{}|"
-print(formatting_short.format("class","280+","285+","290+", "295+"))
-print("|:-|:-|:-|:-|:-|")
+formatting_short = "|{}\t\t\t\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|"
+formatting_long = "|{}\t\t\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|"
+formatting_longer = "|{}\t\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|"
+print(formatting_short.format("class","270+","275+","280+","285+","290+", "295+","300"))
+print("|:-|:-|:-|:-|:-|:-|:-|:-|")
 
 for i in class_name:
-    for j in range(4):
+    for j in range(7):
         if i not in classes_all[j]:
             classes_all[j][i] = 0
-    if len(i)< 8:
-        print(formatting_short.format(i,classes_all[0][i],classes_all[1][i],classes_all[2][i],classes_all[3][i]))
+    if len(i)< 7:
+        print(formatting_short.format(i,classes_all[0][i],classes_all[1][i],classes_all[2][i],classes_all[3][i],classes_all[4][i],classes_all[5][i],classes_all[6][i]))
+    elif len(i)<15:
+        print(formatting_long.format(i,classes_all[0][i],classes_all[1][i],classes_all[2][i],classes_all[3][i],classes_all[4][i],classes_all[5][i],classes_all[6][i]))
     else:
-        print(formatting_long.format(i,classes_all[0][i],classes_all[1][i],classes_all[2][i],classes_all[3][i]))
+        print(formatting_longer.format(i,classes_all[0][i],classes_all[1][i],classes_all[2][i],classes_all[3][i],classes_all[4][i],classes_all[5][i],classes_all[6][i]))
 
 
 
